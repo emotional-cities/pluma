f2de1c750ae753a18f7227406cfff21e5dfac320
Filipe Carvalho <34546039+filcarv@users.noreply.github.com>
2023-04-25 12:31:10 +0100

    Increased fw version to 0.5

--- Firmware/EmotionalCities/app.c
+++ Firmware/EmotionalCities/app.c
@@ -40,7 +40,7 @@ void hwbp_app_initialize(void)
     uint8_t hwH = 0;
     uint8_t hwL = 1;
     uint8_t fwH = 0;
-    uint8_t fwL = 4;
+    uint8_t fwL = 5;
     uint8_t ass = 0;
     
    	/* Start core */
